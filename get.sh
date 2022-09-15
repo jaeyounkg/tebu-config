46da3bc45f62bdc2a8e686ed4386a17095335bff
Jaeyoung <jalpr16@gmail.com>
2022-09-15 17:24:49 +0900

    added tmux conf

--- get.sh
+++ get.sh
@@ -3,5 +3,6 @@ cp ~/.bashrc ./dotfiles/
 cp ~/.gitconfig ./dotfiles/
 cp ~/.vimrc ./dotfiles/
 cp ~/.tebucolemak.vimrc ./dotfiles/
+cp ~/.tmux.conf ./dotfiles/
 cp ~/.config/fish/config.fish ./fish/config.fish
 
